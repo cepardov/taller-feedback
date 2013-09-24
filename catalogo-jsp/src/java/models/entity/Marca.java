c8de1c62fb69c2a0ba304b8f64e87068f18cc3ee
atoncaster@gmail.com <atoncaster@gmail.com@ee22b323-6d81-def5-33df-72f7923e3293>
2013-09-24 21:53:00 +0000

    creacion		auto, marca, modelo en Entity

--- catalogo-jsp/src/java/models/entity/Marca.java
+++ catalogo-jsp/src/java/models/entity/Marca.java
@@ -9,60 +9,26 @@ package models.entity;
  * @author Luis
  */
 public class Marca {
-    protected String rut;
-    protected String nombre;
-    protected String paterno;
-    protected String materno;
-    protected int telefono;
-    protected String email;
+    
+    protected int idmarca;
+    protected String marca;
 
-    public String getRut() {
-        return rut;
+    public int getId() {
+        return idmarca;
     }
 
-    public void setRut(String rut) {
-        this.rut = rut;
+    public void setId(int idmarca) {
+        this.idmarca = idmarca;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getMarca() {
+        return marca;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
-
-    public String getPaterno() {
-        return paterno;
-    }
-
-    public void setPaterno(String paterno) {
-        this.paterno = paterno;
-    }
-
-    public String getMaterno() {
-        return materno;
-    }
-
-    public void setMaterno(String materno) {
-        this.materno = materno;
-    }
-
-    public int getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    
     /* public String toString() {
      * StringBuffer buf = new StringBuffer();
      * buf.append("rut: ");
