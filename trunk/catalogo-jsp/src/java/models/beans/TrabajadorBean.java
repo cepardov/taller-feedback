cf2ba97ba097eff5e28abd924556de7d31f4e10a
cepardov@gmail.com <cepardov@gmail.com@ee22b323-6d81-def5-33df-72f7923e3293>
2013-09-24 23:26:02 +0000

    Creado Trabajador beans, dao, entity + webpage (NO TERMINADO) en modificaciones...

---
+++ trunk/catalogo-jsp/src/java/models/beans/TrabajadorBean.java
@@ -0,0 +1,36 @@
+package models.beans;
+
+import java.util.List;
+import models.dao.TrabajadorDao;
+import models.entity.Trabajador;
+
+public class TrabajadorBean extends Trabajador {
+    
+     private TrabajadorDao trabajadorDao = new TrabajadorDao();
+    
+    public List<Trabajador> findPorNombre() {
+        return trabajadorDao.findPorNombre(nombre);
+    }
+    
+    public List<Trabajador> findAll() {
+        return trabajadorDao.findAll();
+    }
+
+    public Trabajador findByRut() {
+        Trabajador trabajador = null;
+        if (rut != null) {
+            trabajador = trabajadorDao.findByRut(rut);
+        } else {
+            trabajador = new Trabajador();
+        }
+        return trabajador;
+    }
+
+    public void save() {
+        trabajadorDao.save(this);
+    }
+
+    public void delete() {
+        trabajadorDao.delete(this);
+    }
+}
