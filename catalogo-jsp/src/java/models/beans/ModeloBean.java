c6a0bb2bf06ef2de3c695076511d257e953bf57e
atoncaster@gmail.com <atoncaster@gmail.com@ee22b323-6d81-def5-33df-72f7923e3293>
2013-09-24 23:33:06 +0000

    trabajando en DAO

--- catalogo-jsp/src/java/models/beans/ModeloBean.java
+++ catalogo-jsp/src/java/models/beans/ModeloBean.java
@@ -2,35 +2,39 @@ package models.beans;
 
 import java.util.List;
 import models.dao.ClienteDao;
+import models.dao.ModeloDao;
 import models.entity.Cliente;
+import models.entity.Modelo;
 
 public class ModeloBean extends Cliente {
     
-     private ClienteDao clienteDao = new ClienteDao();
+     private ModeloDao modeloDao = new ModeloDao();
     
-    public List<Cliente> findPorNombre() {
-        return clienteDao.findPorNombre(nombre);
+    public List<Modelo> findPorNombre() {
+        return modeloDao.findPorNombre(nombre);
     }
     
-    public List<Cliente> findAll() {
-        return clienteDao.findAll();
-    }
-
-    public Cliente findByRut() {
-        Cliente cliente = null;
-        if (rut != null) {
-            cliente = clienteDao.findByRut(rut);
-        } else {
-            cliente = new Cliente();
-        }
-        return cliente;
-    }
-
-    public void save() {
-        clienteDao.save(this);
-    }
-
-    public void delete() {
-        clienteDao.delete(this);
-    }
+//    public List<Modelo> findAll() {
+//        return modeloDao.findAll();
+//    }
+    
+//Desde aqui esta la parte que los errores
+    
+//  public Modelo findById() {
+//        Modelo modelo = null;
+//        if (id > 0) {
+//            modelo = modeloDao.findById(id);
+//        } else {
+//            modelo = new Modelo();
+//        }
+//        return Modelo;
+//    }
+//
+//    public void save() {
+//        modeloDao.save(this);
+//    }
+//
+//    public void delete() {
+//        modeloDao.delete(this);
+//    }
 }
