32add804c8fc091685dbb08d06c7893fe5dd323f
pofo99@gmail.com <pofo99@gmail.com@ee22b323-6d81-def5-33df-72f7923e3293>
2013-10-02 19:19:40 +0000

    asignacion recrpcion trabajo pt1

---
+++ catalogo-jsp/src/java/models/entity/Asignacion.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models.entity;
+
+/**
+ *
+ * @author adolf
+ */
+public class Asignacion {
+    protected int idasignacion;
+    protected int idficha;
+    protected String rutTrab;
+    protected String fecha;
+    protected String hora;
+
+    public int getIdasignacion() {
+        return idasignacion;
+    }
+
+    public void setIdasignacion(int idasignacion) {
+        this.idasignacion = idasignacion;
+    }
+
+    public int getIdficha() {
+        return idficha;
+    }
+
+    public void setIdficha(int idficha) {
+        this.idficha = idficha;
+    }
+
+    public String getRutTrab() {
+        return rutTrab;
+    }
+
+    public void setRutTrab(String rutTrab) {
+        this.rutTrab = rutTrab;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+    
+    
+    
+    
+    
+    
+}
