7a15ca77bfb78af56306571b52aa69924ef1ffc5
pofo99@gmail.com <pofo99@gmail.com@ee22b323-6d81-def5-33df-72f7923e3293>
2013-10-02 22:48:38 +0000

    asignacionTrabajo modificado

--- trunk/catalogo-jsp/src/java/models/beans/AsignacionBean.java
+++ trunk/catalogo-jsp/src/java/models/beans/AsignacionBean.java
@@ -8,6 +8,43 @@ package models.beans;
  *
  * @author Lab
  */
-public class AsignacionBean {
+
+import java.util.List;
+import models.dao.AsignacionDao;
+import models.entity.Asignacion;
+
+
+public class AsignacionBean extends Asignacion{
     
+    private AsignacionDao asignacionDao = new AsignacionDao();
+    
+    public List<Asignacion> findAll(){
+        
+        return asignacionDao.findAll();
+    }
+    
+    public Asignacion findById()
+    {
+        Asignacion asignacion = null;
+        if (idasignacion != 0)
+        {
+            asignacion = asignacionDao.findById(idasignacion);
+        } else {
+            asignacion = new Asignacion();
+        }
+    
+        return asignacion;
+    }
+    
+    public void save(){
+        asignacionDao.save(this);
+    }
+    public void update()
+    {
+        asignacionDao.update(this);
+    }
+    
+    public void delete(){
+        asignacionDao.delete(this);
+    }
 }
